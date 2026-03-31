f979210ebc5caa06f4f46c447e506769e1bc27cd
Tony Wu <48823951+ichbintonywu@users.noreply.github.com>
2026-03-31 11:55:04 +0800

    Update SemanticRouterServiceTester.java

--- src/main/java/com/redisdemo/sb/Services/SemanticRouterServiceTester.java
+++ src/main/java/com/redisdemo/sb/Services/SemanticRouterServiceTester.java
@@ -15,12 +15,7 @@ public class SemanticRouterServiceTester {
         SemanticRouterService router = new SemanticRouterService(jedis,apiKey);
         RouteMatch routeMatch = router.build().route("Can you tell me about the latest in artificial intelligence?");
         System.out.println(routeMatch);
-//
-//        RouteMatch routeMatch2 = router.build().route("Tell my about Michael Jordan?");
-//        System.out.println(routeMatch2);
 
-//        List<Map<String, Object>> refs = router.build().getRouteReferences("technology",null,null);
-//        System.out.println(refs);
         System.out.println(router.getRouter());
     }
 }
