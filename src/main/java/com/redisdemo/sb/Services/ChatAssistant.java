cd56de5ed5b57625e7db23460cd06991ab7f96e6
Tony Wu <48823951+ichbintonywu@users.noreply.github.com>
2026-03-31 11:52:10 +0800

    Delete src/main/java/com/redisdemo/sb/Services/ChatAssistant.java

--- src/main/java/com/redisdemo/sb/Services/ChatAssistant.java
+++
@@ -1,9 +0,0 @@
-package com.redisdemo.sb.Services;
-
-import dev.langchain4j.service.SystemMessage;
-
-public interface ChatAssistant {
-
-    @SystemMessage("You are a helpful assistant.")
-    String chat(String userMessage);
-}
